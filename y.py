1f7dd2b28b0d4d7d9e261c3fb87d8e0bc800633f
Pan-cham <e.adarsh06@gmail.com>
2026-02-27 16:46:20 +0530

    Implement KNN classification in y.py
    Added KNN classification program using sklearn.

--- y.py
+++ y.py
@@ -1,1 +1,9 @@
-print("hello world")
+"""KNN classification program"""
+
+from sklearn.neighbors import KNeighborsClassifier
+X = [[30],[39],[40],[50],[60],[20],[10],[70]]
+Y = [0,0,1,1,1,0,0,1]
+classifier= KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2 )
+classifier.fit(X,Y)
+X_marks=[[50]]
+print(classifier.predict(X_marks))
